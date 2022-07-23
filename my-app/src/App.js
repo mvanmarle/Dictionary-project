12a88a96e360c546fe7b760a695c3a233f3d970b
mvanmarle <97441731+mvanmarle@users.noreply.github.com>
2022-07-23 14:43:45 +0200

    Added title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import logo from "./logo.png";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello world </h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <footer>Coded by Maureen van marle</footer>
+      </div>
     </div>
   );
 }
